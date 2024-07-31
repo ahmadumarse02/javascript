c34c8c771043c2c49de0c3c7ae9a3964d020bc6d
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-07-31 22:05:35 +0500

    undefined data type

--- JavaScript Fundamentals/Data Types/Introducation.js
+++ JavaScript Fundamentals/Data Types/Introducation.js
@@ -10,3 +10,22 @@
     7)  bigint
 
 */
+
+/*
+
+    JavaScript is a dynamically typed language,
+    meaning that a variable isn’t associated with a specific type. 
+    In other words, a variable can hold a value of different types.
+    To determine the current type of the value stored in a variable,
+    you use the typeof operator:
+
+*/
+
+let counter = 120;
+console.log(typeof(counter)); // "number"
+
+counter = false; 
+console.log(typeof(counter)); // "boolean"
+
+counter = "Hi";
+console.log(typeof(counter)); // "string"
---
+++ JavaScript Fundamentals/Data Types/undefined.js
@@ -0,0 +1,15 @@
+/*
+
+The undefined type:
+    The undefined type is a primitive type that has only one value undefined. 
+    By default, when a variable is declared but not initialized, 
+    it defaults to undefined.
+
+Consider the following example:
+
+*/
+
+
+let counter;
+console.log(counter);        // undefined
+console.log(typeof counter); // undefined
