f89d8d66afe87f6d99d1fc3eaa7022dc8c9e4342
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-07-31 20:41:23 +0500

    Syntax

---
+++ JavaScript Fundamentals/Syntax.js
@@ -0,0 +1,19 @@
+/*
+    JavaScript engine ignores whitespace. 
+        However, you can use whitespace to format the code to make it easy to read and maintain.
+
+    The following JavaScript code doesn’t use whitespace:
+
+*/
+
+let formatted = true; if (formatted) {console.log('The code is difficult to read');}
+
+
+
+//It is equivalent to the following code that uses whitespace. Hence, this code is much easier to read:
+
+let formatted1 = true;
+
+if (formatted) {
+  console.log('The code is easy to read');
+}
