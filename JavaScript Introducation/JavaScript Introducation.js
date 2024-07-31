336702ac01ffc490e870e19a82f1616645163c71
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-07-31 19:49:28 +0500

    JavaScript Introducation

---
+++ JavaScript Introducation/JavaScript Introducation.js
@@ -0,0 +1,14 @@
+/*
+
+JavaScript is a programming language initially designed to interact with elements of web pages.
+Within web browsers, JavaScript consists of three main parts:
+
+    1)  ECMAScript provides the core functionality.
+    2)  The Document Object Model (DOM) provides interfaces for interacting with elements on web pages
+    3)  The Browser Object Model (BOM) provides the browser API for interacting with the web browser.
+
+History:
+    In 1995, JavaScript was developed by Brendan Eich, a Netscape developer.
+    Initially named Mocha, it was later renamed to LiveScript and it was also renamed to JavaScript.
+
+*/
